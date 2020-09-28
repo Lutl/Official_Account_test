b3c8b99710a253a42441a613df11e9c0854c4cdf
lutl <lutl1024@gmail.com>
2020-09-28 16:34:22 +0800

    解决调试时无法使用80端口的问题

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ urls = (
 )
 
 
+class MyApplication(web.application):
+    def run(self, port=8080, *middleware):
+        func = self.wsgifunc(*middleware)
+        return web.httpserver.runsimple(func, ('0.0.0.0', port))
+
+
 if __name__ == '__main__':
-    app = web.application(urls, globals())
-    app.run()
+    # app = web.application(urls, globals())
+    # TODO: 需要解决默认使用8080端口启动项目的问题
+    app = MyApplication(urls, globals())
+    app.run(port=80)
 
 # import web
 
